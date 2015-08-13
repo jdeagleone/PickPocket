3ff685560ab5e94e77b40160e96ee17d38fd4857
Garett Dunn <garettmd@gmail.com>
2015-08-13 16:35:18 -0500

    Updates to main.html and corresponding views

--- app/auth_module/views.py
+++ app/auth_module/views.py
@@ -1,7 +1,7 @@
 import json
 import webbrowser
 from app import app
-from config import CONSUMER_KEY, HEADERS, REDIRECT_URI, AUTH_URL, OAUTH_ACCESS_URL, OAUTH_REQUEST_URL
+from config import CONSUMER_KEY, HEADERS, REDIRECT_URI, AUTH_URL, OAUTH_ACCESS_URL, OAUTH_REQUEST_URL, GET_URL
 from flask import render_template, g
 import requests as r
 
@@ -14,20 +14,35 @@ def front():
 
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
+    # Step 2 of Pocket developer documentation
     data = {'consumer_key': CONSUMER_KEY, 'redirect_uri': REDIRECT_URI}
     response = r.post(OAUTH_REQUEST_URL, headers=HEADERS, data=json.dumps(data))
     json_response = json.loads(response.text)
     final_auth_url = AUTH_URL + '?request_token=' + json_response[
         'code'] + '&redirect_uri=' + REDIRECT_URI + '/' + json_response['code']
+    # Step 3 of Pocket developer documentation
     webbrowser.open_new_tab(final_auth_url)
     return True
 
 
+# Step 4 of Pocket developer documentation
 @app.route('/authorize/<code>')
 def authorize(code):
+    # Step 5 of Pocket developer documentation
     data = {'consumer_key': CONSUMER_KEY, 'code': code}
     auth_response = r.post(OAUTH_ACCESS_URL, headers=HEADERS, data=json.dumps(data))
     json_response = json.loads(auth_response.text)
     g.access_token = json_response['access_token']
     g.user = json_response['username']
-    return render_template('main.html')
+
+    # Initial retrieval of 10 latest Pocket articles
+    retr_data = {'count': 10, 'sort': 'newest', 'detailType': 'simple', 'consumer_key': CONSUMER_KEY,
+                 'access_token': g.access_token}
+    articles = r.post(GET_URL, headers=HEADERS, data=json.dumps(retr_data))
+    articles_json = json.loads(articles.text)
+    articles_list = []
+    for x in articles_json['list']:
+        articles_list.append(articles_json['list'][x]['resolved_title'])
+    return render_template('main.html',
+                           articles=articles_list
+                           )
