89c85563040cfd8836b99cc02704aa49b3c5884c
Garett Dunn <garettmd@gmail.com>
2015-08-19 16:29:34 -0500

    heroku fixes

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+
+port = int(os.environ.get("PORT", 5000))
+
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 THREADS_PER_PAGE = 2
@@ -7,7 +11,7 @@ CSRF_ENABLED = True
 CSRF_SESSION_KEY = "secret"
 SECRET_KEY = "secret"
 CONSUMER_KEY = '41815-f92e2de9fd4887e04058494b'
-REDIRECT_URI = 'http://127.0.0.1:5000/authorize'
+REDIRECT_URI = 'http://0.0.0.0:%s/authorize' % port
 OAUTH_REQUEST_URL = 'https://getpocket.com/v3/oauth/request'
 AUTH_URL = 'https://getpocket.com/auth/authorize'
 OAUTH_ACCESS_URL = 'https://getpocket.com/v3/oauth/authorize'
