0b8ca7817c28d40aebc019b9c2f9074083772976
Garett Dunn <garettmd@gmail.com>
2015-08-19 22:35:00 -0500

    Updated config to use heroku URL

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
-
 port = int(os.environ.get("PORT", 5000))
-
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
@@ -11,7 +9,7 @@ CSRF_ENABLED = True
 CSRF_SESSION_KEY = "secret"
 SECRET_KEY = "secret"
 CONSUMER_KEY = '41815-f92e2de9fd4887e04058494b'
-REDIRECT_URI = 'http://0.0.0.0:%s/authorize' % port
+REDIRECT_URI = 'http://pick-pocket.herokuapp.com%s/authorize' % port
 OAUTH_REQUEST_URL = 'https://getpocket.com/v3/oauth/request'
 AUTH_URL = 'https://getpocket.com/auth/authorize'
 OAUTH_ACCESS_URL = 'https://getpocket.com/v3/oauth/authorize'
